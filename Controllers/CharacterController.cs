330534e05ccb516e5304f994e9bf4996b1134f62
skathix <32309102+skathix@users.noreply.github.com>
2021-10-27 23:18:33 -0500

    enhancement/3: Stubbed out Controllers

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using DICE_CharacterTracker.Data;
+using DICE_CharacterTracker.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DICE_CharacterTracker.Controllers
+{
+    public class CharacterController : ControllerBase
+    {
+        private DiceDbContext _dbContext;
+
+        public CharacterController(DiceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // Create
+
+        public Character CreateCharacter(Character newCharacter)
+        {
+            throw new NotImplementedException();
+        }
+        
+        // Read
+
+        public List<Character> GetAllCharacters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Character> GetCharactersForUserId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        // Update
+
+        public Character UpdateCharacter(int id, Character character)
+        {
+            throw new NotImplementedException();
+        }
+        
+        // Delete
+
+        public Character DeleteCharacter(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
---
+++ Controllers/CharacterSheetStateController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using DICE_CharacterTracker.Data;
+using DICE_CharacterTracker.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DICE_CharacterTracker.Controllers
+{
+    public class CharacterSheetStateController : ControllerBase
+    {
+        private DiceDbContext _dbContext;
+
+        public CharacterSheetStateController(DiceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // Create
+
+        public CharacterSheetState CreateCharacterSheetState(CharacterSheetState newCharacterSheetState)
+        {
+            throw new NotImplementedException();
+        }
+        
+        // Read
+
+        public List<CharacterSheetState> GetAllCharacterSheetStates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CharacterSheetState> GetCharacterSheetStatesForCharacterId(int characterId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CharacterSheetState GetCharacterSheetState(int id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        // Update
+
+        public CharacterSheetState UpdateCharacterSheetState(CharacterSheetState characterSheetState)
+        {
+            throw new NotImplementedException();
+        }
+        
+        // Delete
+
+        public CharacterSheetState DeleteCharacterSheetState(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
